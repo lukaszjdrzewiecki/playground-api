30762d4fa6bc266303ce225aa13480b76380f99f
Kamil Kiewisz <kamkie@outlook.com>
2018-03-08 00:44:44 +0100

    better hello endpoint

--- src/main/java/p76/playground/rest/HelloController.java
+++ src/main/java/p76/playground/rest/HelloController.java
@@ -1,19 +1,15 @@
 package p76.playground.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import p76.playground.db.School;
-import p76.playground.rest.SchoolRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home(String name) {
-        return "Hello World! " + name;
+    @RequestMapping("/hello")
+    public String home(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello " + name + "!";
     }
 
 
